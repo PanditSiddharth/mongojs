9b78738c03b36fe56fd0bd637a137d7265970a95
sidhelpbot <mynmss03@gmail.com>
2023-01-18 11:56:24 +0530

    mongo

--- functions/bot/cn.js
+++ functions/bot/cn.js
@@ -1,10 +1,10 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 async function f(bot, mdb) {
-  const client = new MongoClient(mdb, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  const client = await new MongoClient(mdb, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
   try {
     await client.connect(async err => {
-      const collection = client.db("test").collection("devices");
+      const collection = await client.db("test").collection("devices");
       // perform actions on the collection object
       // collection.insertOne({"name: ": "sdid"}).then((res1) => console.log(res1))
 
@@ -15,9 +15,9 @@ async function f(bot, mdb) {
       //   console.log(res1)
       // }
       // )
-      collection.findOne({ "name: ": "sdid" }).then((res1) => {
+      await collection.findOne({ "name: ": "sdid" }).then(async (res1) => {
         console.log(res1)
-        bot.telegram.sendMessage('@shabdt', JSON.stringify(res1));
+    await bot.telegram.sendMessage('@shabdt', JSON.stringify(res1));
       }
       )
     })
