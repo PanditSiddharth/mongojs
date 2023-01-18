9af7b7e65a144e5198e1aec003b56eb9dab43d49
sidhelpbot <mynmss03@gmail.com>
2023-01-18 10:51:46 +0530

    mongo

---
+++ functions/bot/cn.js
@@ -0,0 +1,33 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config
+
+async function f(bot, mdb) {
+  const uri = process.env.MDB || mdb;
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  try {
+    client.connect(async err => {
+      const collection = client.db("test").collection("devices");
+      // perform actions on the collection object
+      // collection.insertOne({"name: ": "sdid"}).then((res1) => console.log(res1))
+
+      // collection.deleteOne({"name: ": "sdid"}).then((res1) => console.log(res1))
+
+      await collection.findOne({ "name: ": "sdid" }).then((res1) => {
+        bot.telegram.sendMessage('@shabdt', JSON.stringify(res1));
+        console.log(res1)
+      }
+      )
+      collection.findOne({ "name: ": "sdid" }).then((res1) => {
+        console.log(res1)
+        bot.telegram.sendMessage('@shabdt', JSON.stringify(res1));
+      }
+      )
+    })
+  } finally {
+
+    await client.close();
+    console.log('connection Closed')
+    return
+  }
+}
+module.exports = f
--- functions/bot/start.js
+++ functions/bot/start.js
@@ -1,4 +1,9 @@
-async function strt(bot) {
+const axios = require('axios');
+const f = require('./cn.js')
+const { message } = require('telegraf/filters');
+
+
+async function strt(bot, mdb) {
 
   try{
 
@@ -6,8 +11,13 @@ async function strt(bot) {
     ctx.reply("I am javascript bot with mongo db\nWorking in testing mode..")
   })
 
-  bot.start(ctx => {
-    ctx.reply("I am javascript bot with mongo db\nWorking in testing mode..")
+  // bot.start(ctx => {
+  //   ctx.reply("I am javascript bot with mongo db\nWorking in testing mode..")
+  // })
+
+  bot.on(message('text'),async (ctx) => {
+    await f(bot, mdb);
+    console.log('stopped');
   })
 
   } catch (e) {
