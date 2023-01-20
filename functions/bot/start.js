b13b2874752d10ad5f055bf1c7f65db074b008bd
sidhelpbot <mynmss03@gmail.com>
2023-01-20 18:35:38 +0530

    del

--- functions/bot/start.js
+++ functions/bot/start.js
@@ -11,16 +11,27 @@ async function strt(bot, mdb) {
     })
 
    
-    bot.command('st', async (ctx) => {
-      try {
-              let y = await ctx.reply('Bot starting..',).then((msg)=>{ 
-                setTimeout(() => {  
-                bot.telegram.editMessageText(ctx.chat.id, msg.message_id, undefined, 'yo')
-              }, 2000)})
-             
-      } catch (e) {
-        ctx.reply("Error occured")
-      }
+    bot.command('st', (ctx) => { 
+      let t = 1;
+      let m = ctx.message.message_id
+
+      let del = async (ctx) => { 
+      let mid = m - ((10 - t) * 10)
+      console.log(mid)
+      for (let i = 0; i < 9; i++) {
+      bot.telegram.deleteMessage(ctx.chat.id, mid + i)
+      .then((y)=> console.log(y + mid))
+      .catch(error => {
+          // console.error(error)
+          ctx.reply("Error occured" + error.message)
+      });
+    }
+  }
+
+  for (let j = 0; j < 10; j++) {
+    del(ctx)
+  }
+
     })
 
     bot.start(async (ctx) => {
@@ -55,7 +66,7 @@ await bot.telegram.editMessageText(ctx.chat.id, msg.message_id, undefined, arr[i
 })
 
 
-    await bot.on(message('sticker'), async (ctx) => {
+   await bot.on(message('sticker'), async (ctx) => {
       let str = 'this bot is starting soon please wait while i am doing some processes in our backend for responding you in good manner which you want to see'
       let msgg = []
       msgg = str.split(' ')
