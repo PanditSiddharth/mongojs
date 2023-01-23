f357cc9af29c1f350bc899deb91654f74035c462
sidhelpbot <mynmss03@gmail.com>
2023-01-23 21:40:33 +0530

    del

--- functions/bot/adactions/unmute.js
+++ functions/bot/adactions/unmute.js
@@ -37,6 +37,7 @@ const unmute = async (bot, ctxx, cn = -1, op1 = -1, op2 = -1, op3 = -3) => {
         if (ctxx.message.reply_to_message) {
             try {
                 n = await bot.telegram.promoteChatMember(chat_id = ctxx.chat.id, user_id = ctxx.message.reply_to_message.from.id, can_send_messages = false, can_add_web_page_previews = true)
+                await sleep(1000)
             } catch (err) {
                 try {
                     return ctxx.reply(err.message)
