2e60684941f669050520866e52a4eee41e27fdfa
sidhelpbot <mynmss03@gmail.com>
2023-01-23 13:23:56 +0530

    del

--- functions/bot/adactions/unmute.js
+++ functions/bot/adactions/unmute.js
@@ -1,7 +1,7 @@
 const unban = require("./unban");
 const unmute = (bot, ctxx, cn = -1, op1 = -1, op2 = -1, op3 = -3) => {
     try {
-        unban(bot, ctxx, { unmute: true})
+        unban(bot, ctxx, { unmute: true })
         } catch (error) {
             ctxx.reply('Error: ' + error.message)
         }
--- functions/bot/allactions.js
+++ functions/bot/allactions.js
@@ -14,90 +14,105 @@ const help = require('./adactions/help.js')
 const allactions = async (bot) => {
     console.log('allactions')
     try {
-        let mem;
-        await bot.use(
-            // ctx.admin()
-            
+        // let mem = { status: false};
+        let ctx;
+        let adm = false;
+
+        bot.use(
             async (ctx, next) => {
-            
                 if (ctx.message) {
-                  
-                    mem = await bot.telegram.getChatMember(ctx.message.chat.id, ctx.message.from.id)
-                   .catch((err)=> {return ctx.reply('Error getChatMember' + err.message)})
-                    if (mem.status === 'creator' || mem.status === 'administrator') {
-                       
-                        try {
-                            
-                        await bot.hears('run', ctx => { ctx.reply('running in test mode') })
 
-                        bot.command('ndel', async (ctx, next) => {
-                            del(bot, ctx)
-                            // next(ctx)
-                            
-                        })
+                    let mem = await bot.telegram.getChatMember(ctx.message.chat.id, ctx.message.from.id)
+                        .catch((err) => { return ctx.reply('Error getChatMember' + err.message) })
 
-                        bot.command('nunmute', async (ctx, next) => {
-                            unmute(bot, ctx)
-                            // next(ctx)
-                        })
-                        
-                        bot.command('nmute', async (ctx, next) => {
-                            mute(bot, ctx)
-                            // next(ctx)
-                        })
-
-                        bot.command('npurge', async (ctx, next) => {
-                            purge(bot, ctx)
-                            // next(ctx)
-                        })
-
-                        bot.command('restrict', async (ctx, next) => {
-                            restrict(bot, ctx)
-                            // next(ctx)
-                        })
-
-                        bot.command('nkick', async (ctx, next) => {
-                            kick(bot, ctx)
-                            // next(ctx)
-                        })                        
-                        bot.command('dban', async (ctx, next) => {
-                            dban(bot, ctx)
-                            // next(ctx)
-                        })                        
-                        bot.command('npromote', async (ctx, next) => {
-                            promote(bot, ctx)
-                            // next(ctx)
-                        })
-
-                        bot.command('ndemote', async (ctx) => {
-                            demote(bot, ctx)
-                        })
-
-                        bot.command('nban', async (ctx, next) => {
-                            ban(bot, ctx)
-                        })                       
-                        
-                        bot.command('nunban', async (ctx, next) => {
-                            unban(bot, ctx)
-                        }) 
-                       bot.command('nhelp', async (ctx, next) => {
-                            help(bot, ctx)
-                        })
-
-                    } catch (errr) {
-                        ctx.reply("error " + errr.message)
-                    }
+                    if (mem.status == 'creator' || mem.status == 'administrator'){
+                        adm = true
+                    }else {
+                        adm = false
 
                     }
-                    else{
-                        // ctx.reply('its members message')
-                        console.log('yo')
-                    }
+                    next(ctx)
                 }
-                next(ctx)
-            }
+                else
+                    return
+            })
 
-        )
+            bot.on('message', (ctx, next) => { console.log(adm); next(ctx)})
+
+        try {
+
+            await bot.hears('run', ctx => { ctx.reply('running in test mode') })
+
+            bot.command('ndel', async (ctx, next) => {
+                if (adm)
+                del(bot, ctx)
+                // next(ctx)
+
+            })
+
+            bot.command('nunmute', async (ctx, next) => {
+                if (adm)
+                unmute(bot, ctx)
+                // next(ctx)
+            })
+
+            bot.command('nmute', async (ctx, next) => {
+                if (adm)
+                mute(bot, ctx)
+                // next(ctx)
+            })
+
+            bot.command('npurge', async (ctx, next) => {
+                if (adm)
+                purge(bot, ctx)
+                // next(ctx)
+            })
+
+            bot.command('restrict', async (ctx, next) => {
+                if (adm)
+                restrict(bot, ctx)
+                // next(ctx)
+            })
+
+            bot.command('nkick', async (ctx, next) => {
+                if (adm)
+                kick(bot, ctx)
+                // next(ctx)
+            })
+            bot.command('dban', async (ctx, next) => {
+                if (adm)
+                dban(bot, ctx)
+                // next(ctx)
+            })
+            bot.command('npromote', async (ctx, next) => {
+                if (adm)
+                promote(bot, ctx)
+                // next(ctx)
+            })
+
+            bot.command('ndemote', async (ctx) => {
+                if (adm)
+                demote(bot, ctx)
+            })
+
+            bot.command('nban', async (ctx, next) => {
+                if (adm)
+                ban(bot, ctx)
+            })
+
+            bot.command('nunban', async (ctx, next) => {
+                if (adm)
+                unban(bot, ctx)
+            })
+
+            bot.command('nhelp', async (ctx, next) => {
+                if (adm)
+                help(bot, ctx)
+            })
+
+        } catch (errr) {
+            ctx.reply("error " + errr.message)
+        }
 
     } catch (error) {
         console.log(error)
