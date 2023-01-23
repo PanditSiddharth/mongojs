a1a505c4ead06e5fd6833b74b466bda11785ce18
sidhelpbot <mynmss03@gmail.com>
2023-01-23 21:36:38 +0530

    del

--- functions/bot/adactions/unmute.js
+++ functions/bot/adactions/unmute.js
@@ -35,20 +35,22 @@ const unmute = async (bot, ctxx, cn = -1, op1 = -1, op2 = -1, op3 = -3) => {
 
     try {
         if (ctxx.message.reply_to_message) {
-            n = await bot.telegram.promoteChatMember(chat_id = ctxx.chat.id, user_id = ctxx.message.reply_to_message.from.id, can_send_messages = false, can_add_web_page_previews = true)
-                .catch(async (err) => {
-                    try {
-                        return ctxx.reply(err.message)
-                    } catch (error) {
-                    }
-                });
+            try {
+                n = await bot.telegram.promoteChatMember(chat_id = ctxx.chat.id, user_id = ctxx.message.reply_to_message.from.id, can_send_messages = false, can_add_web_page_previews = true)
+            } catch (err) {
+                try {
+                    return ctxx.reply(err.message)
+                } catch (error) {
+                }
+            };
 
-            await sleep(300)
+            await sleep(500)
             if (n != true)
                 return
-            
-            if(n == true)
-            await ctxx.reply(ctxx.message.reply_to_message.from.first_name + " now you can speak")
+
+            await sleep(500)
+            if (n == true)
+                await ctxx.reply(ctxx.message.reply_to_message.from.first_name + " now you can speak")
 
             await sleep(100)
             console.log(n)
