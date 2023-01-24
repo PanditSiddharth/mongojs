cb22c4d79c35525d7adf58912caf0cf317a3e47f
sidhelpbot <mynmss03@gmail.com>
2023-01-24 18:53:56 +0530

    del

--- functions/bot/adactions/unmute.js
+++ functions/bot/adactions/unmute.js
@@ -5,7 +5,7 @@ const unmute = async (bot, ctxx, cn = -1, op1 = -1, op2 = -1, op3 = -3) => {
 
     try {
         ctxx.state.rmem = await bot.telegram.getChatMember(ctxx.message.chat.id, ctxx.message.reply_to_message.from.id)
-        // await ctxx.state.sleep(10)
+        await ctxx.state.sleep(100)
     } catch (err) {
         ctxx.state.rmem = false
         return ctxx.reply('Umt admm error: ' + err.message)
@@ -14,8 +14,10 @@ const unmute = async (bot, ctxx, cn = -1, op1 = -1, op2 = -1, op3 = -3) => {
     if (ctxx.state.rmem == false)
         return
 
+        await ctxx.state.sleep(100)
     if (ctxx.state.rmem.status == 'creator' || ctxx.state.rmem.status == 'administrator') {
         ctxx.state.admm = true
+        await ctxx.state.sleep(100)
         return ctxx.reply(ctxx.message.reply_to_message.from.first_name + ' is admin and can speak already')
     } else {
         ctxx.state.admm = false
